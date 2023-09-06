e8e341b96aca52796df87255a9f7beb266e2f582
Saurodip Sinha <saurodip172005@gmail.com>
2023-09-06 23:49:16 +0530

    code update

--- problem-4.c
+++ problem-4.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 void main(){
     float r,b;
-    float d=3.14;
     printf("Enter the radius of the circle:");
     scanf("%f",&r);
-    b= d*r*r;
-
-    printf("The answer is:%f",b);
+    b= 3.14*r*r;
+    printf("The area is:%.2f",b);
 }
