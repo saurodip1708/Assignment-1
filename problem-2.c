28eaa3e31a8882436c3c4ce8e4a9a7c7c7376245
Saurodip Sinha <saurodip172005@gmail.com>
2023-09-06 21:40:59 +0530

    updated code

--- problem-2.c
+++ problem-2.c
@@ -2,43 +2,23 @@
 #include<stdio.h>
 void main(){
     //variables
-    int a,b,c,d,m;
-
-    float e,f,g;
-    //addition
-    printf("The code to sum of two number\n");
+    float a,b,c,d,e,f;
+    printf("The code to perform operations in c\n");
     printf("Enter the first no:");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("Enter the second no:");
-    scanf("%d",&b);
+    scanf("%f",&b);
+
     c=a+b;
-    printf("The sum is %d\n",c);
+    printf("The sum is %.2f\n",c);
 
-    //substraction
-    printf("The code to subtract two number\n");
-    printf("Enter the first no:");
-    scanf("%d",&a);
-    printf("Enter the second no:");
-    scanf("%d",&b);
     d=b-a;
-    printf("The difference is %d\n",d);
+    printf("The difference is %.2f\n",d);
 
-    //multiplication
-    printf("The code to multiply two number\n");
-    printf("Enter the first no:");
-    scanf("%d",&a);
-    printf("Enter the second no:");
-    scanf("%d",&b);
-    m=b*a;
-    printf("The multiplication is %d\n",m);
+    e=b*a;
+    printf("The multiplication is %.2f\n",e);
 
-    //division
-    printf("The code to divide two number\n");
-    printf("Enter the first no:");
-    scanf("%d",&f);
-    printf("Enter the second no:");
-    scanf("%d",&g);
-    e=f/g;
-    printf("The quotient is %f",e);
+    f=a/b;
+    printf("The quotient is %.2f",f);
 
 }
